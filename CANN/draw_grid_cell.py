950b6de216ee93fde40c1d6300a9dabba4cbda1d
BoyangL1 <liboyang0209@163.com>
2024-01-10 16:20:21 +0800

    update transformer parameters

--- CANN/draw_grid_cell.py
+++ CANN/draw_grid_cell.py
@@ -2,8 +2,9 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from tqdm import tqdm
 
-file_path = './data/place_grid_data.pkl'
+file_path = './data/coords_grid_data.pkl'
 
 # Open the file in binary read mode
 with open(file_path, 'rb') as file:
@@ -19,13 +20,13 @@ def visualize_and_save_grid_cells(data, directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for i, (key, value) in enumerate(data.items()):
+    for i, (key, value) in enumerate(tqdm(data.items())):
         if isinstance(value, (list, np.ndarray)) and len(np.array(value).shape) == 3:
             plt.figure()
-            plt.imshow(value[0, :, :], cmap='hot', interpolation='nearest')
+            plt.imshow(value[4, :, :], cmap='hot', interpolation='nearest')
             plt.title(f"Grid Cell for Coordinate {key}")
             plt.colorbar()
-            file_name = os.path.join(directory, f'grid_cell_first_layer_{i}.png')
+            file_name = os.path.join(directory, f'grid_cell_{i}.png')
             plt.savefig(file_name)
             plt.close()
         else:
